8061541cb693fecbfe656486658a8b6ff654ff4e
Oscar Fonseca <o.fonsecan@uniandes.edu.co>
2023-03-11 01:28:55 -0500

    CI-DEVELOP: Flujo CI Develop 32

--- tests/test_persona.py
+++ tests/test_persona.py
@@ -42,3 +42,6 @@ class PersonaTestCase(unittest.TestCase):
    def test_instancia_clase(self):
       self.assertIsInstance(self.persona1, Persona)
       self.assertNotIsInstance(self.grupo, Persona)
+
+   def paila(self):
+      self.assertEqual(1, 2)
